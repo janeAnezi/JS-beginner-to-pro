bc1597dc785091c799624f055069486126e1d733
Anezijane <ahnnieanezi@gmail.com>
2023-12-28 10:08:31 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+//const content = document.getElementById('test').innerHTML = '<h1>Welcome To JavaScript Beginner to Pro</h1>'
+
+/*************** variables and data types */
+// var firstName = 'John';
+// console.log(firstName);
+
+// var lastName = 'Doe';
+// var age = 26;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+// job = 'teacher';
+// console.log(job);
+
+/******* Variable mutation and type coercion */
+var firstName = "john";
+var age = 26;
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'driver';
+isMarried = true;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job +'.' + 'Is he married? ' + isMarried + '.' )
+
+// Variable mutation
+age = 'twenty six';
+job = 'teacher';
