5218f4d6f91866d605a8af8cd38b54b0f96d1ec4
Anezijane <ahnnieanezi@gmail.com>
2023-12-30 10:56:14 +0100

    initial commit

--- main.js
+++ main.js
@@ -16,17 +16,33 @@
 // console.log(job);
 
 /******* Variable mutation and type coercion */
-var firstName = "john";
-var age = 26;
+/*var firstName = "john";
+var age = 26;*/
 // Type coercion
-console.log(firstName + ' ' + age);
+/*console.log(firstName + ' ' + age);
 
 var job, isMarried;
 job = 'driver';
 isMarried = true;
 
 console.log(firstName + ' is a ' + age + ' year old ' + job +'.' + 'Is he married? ' + isMarried + '.' )
-
+*/
 // Variable mutation
-age = 'twenty six';
+/*age = 'twenty six';
 job = 'teacher';
+console.log('This should work: ', firstName + ' is a ' + age + ' year old ' + job + '.');
+var lastName = prompt('what is his last name?');
+alert(firstName +' '+ lastName);*/
+
+/********* Basic operators */
+var currentYear,johnYear,markYear;
+ currentYear = 2023;
+ ageJohn = 26;
+ ageMark = 24;
+
+ /** math operator */
+ johnYear = currentYear - ageJohn;
+ markYear = currentYear - ageMark;
+console.log("John was born in ", johnYear, " and Mark, john's friend was born in", markYear);
+ var isJohnOlder = ageJohn > ageMark;
+ console.log(isJohnOlder);
