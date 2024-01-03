62fc7c31d5a07207e965f15f342d7f38baf8eb53
Anezijane <ahnnieanezi@gmail.com>
2024-01-03 16:29:15 +0100

    add switch statement

--- main.js
+++ main.js
@@ -107,11 +107,48 @@ if(isMarried) {
 }
 
 /**** Boolean logic */
-var age = 18;
+var age = 15;
 if(age < 18) {
     console.log('you are not old enough to vote');
 }else if (age >= 18 && age <= 20) {
     console.log('you can vote but you are still a teenager');
 }else {
     console.log('you can vote and are an adult!');
+}
+
+/***** Tenary operator */
+var herName = "Aisha";
+var herAge = 20;
+
+herAge <= 18 ? console.log(herName, 'drinks wine') : console.log(herName, 'takes coke');
+var drinks = herAge > 18 ? "wine" : "Malt";
+console.log(drinks);
+
+/****** switch statement */
+var job = "cook";
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log("teaches kids how to code");
+        break;
+    case 'farmer': 
+        console.log('brings fresh vegetables');
+        break;
+    case 'driver': 
+        console.log('takes kids to school');
+        break;
+    default:
+        console.log('does something else');
+}
+
+var age = 25;
+switch(true) {
+    case age < 13:
+        console.log('child');
+        break;
+    case age >= 13 && age <= 20: 
+        console.log('you are not old enough to vote');
+        break;
+    default:
+    console.log('you can vote and are an adult!');        
 }
