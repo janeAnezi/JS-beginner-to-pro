d5ebd21dfb63f06ab64468a05e0d2d0851828cff
Anezijane <ahnnieanezi@gmail.com>
2024-01-04 23:32:26 +0100

    code challenge 3

--- main.js
+++ main.js
@@ -285,4 +285,12 @@ console.log(janeMood);
 /**** coding challenge 3 ****/
 var bills = [124, 48, 268]
 
-
+function tipCalculator(bill) {
+    if (bill < 50) {
+        return bill * .2;
+    }else if (bill >= 50 && bill <= 200) {
+        return bill * .15;
+    }else {
+        return bill * .1;
+    }
+}
