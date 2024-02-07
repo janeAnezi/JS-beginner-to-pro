38a62f8eccaeb3f1d7870e07fbced50f4ece9c27
Anezijane <ahnnieanezi@gmail.com>
2024-02-07 20:36:04 +0100

    quiz Game coding challenge

--- quizGame/script.js
+++ quizGame/script.js
@@ -4,69 +4,103 @@
 // the answers
 //  correctAnswer
 
-let Question = function (questionText,  possibleAnswers, correctAnswer) {
-    this.questionText = questionText;
-    this.possibleAnswers = possibleAnswers;
-    this.correctAnswer = correctAnswer
-};
+// (function() {
+//         let Question = function (questionText,  possibleAnswers, correctAnswer) {
+//         this.questionText = questionText;
+//         this.possibleAnswers = possibleAnswers;
+//         this.correctAnswer = correctAnswer
+//     };
 
-Question.prototype.displayQuestion = function () { 
-    console.log(this.questionText);
+//     Question.prototype.displayQuestion = function () { 
+//         console.log(this.questionText);
 
-    for(let i=1; i< this.possibleAnswers.length; i++){
-        console.log(`${i}:  ${this.possibleAnswers[i]}`);
-    }
-};
-Question.prototype.checkAnswer = function(ans) {
-    if(ans === this.correctAnswer) {
-        console.log('The answer is correct!');
-    }else {
-        console.log('Wrong answer!, Try Again');
-    }
-}
-let question1 = new Question (
-    "What is the capital of France?", 
-    ["London","Paris","Berlin","Rome"], 
-    1
-    );
-let question2 = new Question (
-    "Which planet is known as the Red Planet?",
-    ["Earth", "Mars", "Venus", "Jupiter"],
-    1 
-);
-let question3 = new Question (
-    "Who painted the Mona Lisa?", 
-    ["Leonardo da Vinci", "Michaelangelo", "Donatello",  "Vincent van Gogh"], 
-    0
-);
-let questionArr =  [question1, question2, question3]; // array holding all questions
- 
-let numRandom = Math.floor(Math.random() * questionArr.length);
- questionArr[numRandom].displayQuestion();
-
- let answer = parseInt(prompt("Please select the correct answer"));
-
-questionArr[numRandom].checkAnswer(answer);
-
-
-
-
-
-
-
-    // if(answer == "exit"){
-    //     alert("Goodbye!");
-    // }else if(isNaN(parseInt(answer))){
-    //     console.log("That is not a valid input.");
-    // }
-
-// let correctAns;
-// function Question(questn, arr) {
-//     return function() {
-//         console.log('Question: ' + questn);
-//         for (let i = 0; i < arr.length; i++) {
-//             console.log((i+1) + ') ' + arr[i]);
+//         for(let i=1; i< this.possibleAnswers.length; i++){
+//             console.log(`${i}:  ${this.possibleAnswers[i]}`);
+//         }
+//     };
+//     Question.prototype.checkAnswer = function(ans) {
+//         if(ans === this.correctAnswer) {
+//             console.log('The answer is correct!');
+//         }else {
+//             console.log('Wrong answer!, Try Again');
 //         }
 //     }
+//     let question1 = new Question (
+//         "What is the capital of France?", 
+//         ["London","Paris","Berlin","Rome"], 
+//         1
+//         );
+//     let question2 = new Question (
+//         "Which planet is known as the Red Planet?",
+//         ["Earth", "Mars", "Venus", "Jupiter"],
+//         1 
+//     );
+//     let question3 = new Question (
+//         "Who painted the Mona Lisa?", 
+//         ["Leonardo da Vinci", "Michaelangelo", "Donatello",  "Vincent van Gogh"], 
+//         1
+//     );
+//     let questionArr =  [question1, question2, question3]; // array holding all questions
+    
+//     let numRandom = Math.floor(Math.random() * questionArr.length);
+//     questionArr[numRandom].displayQuestion();
 
-// }
+//     let answer = parseInt(prompt("Please select the correct answer"));
+
+//     questionArr[numRandom].checkAnswer(answer);
+//     })();
+
+    // to display the next random question so that the game never ends
+
+    (function() {
+        let Question = function (questionText,  possibleAnswers, correctAnswer) {
+        this.questionText = questionText;
+        this.possibleAnswers = possibleAnswers;
+        this.correctAnswer = correctAnswer
+    };
+
+    Question.prototype.displayQuestion = function () { 
+        console.log(this.questionText);
+
+        for(let i=1; i< this.possibleAnswers.length; i++){
+            console.log(`${i}:  ${this.possibleAnswers[i]}`);
+        }
+    };
+    Question.prototype.checkAnswer = function(ans) {
+        if(ans === this.correctAnswer) {
+            console.log('The answer is correct!');
+        }else {
+            console.log('Wrong answer!, Try Again');
+        }
+    }
+    let question1 = new Question (
+        "What is the capital of France?", 
+        ["London","Paris","Berlin","Rome"], 
+        1
+        );
+    let question2 = new Question (
+        "Which planet is known as the Red Planet?",
+        ["Earth", "Mars", "Venus", "Jupiter"],
+        1 
+    );
+    let question3 = new Question (
+        "Who painted the Mona Lisa?", 
+        ["Leonardo da Vinci", "Michaelangelo", "Donatello",  "Vincent van Gogh"], 
+        1
+    );
+    let questionArr =  [question1, question2, question3]; // array holding all questions
+    
+        function nextQuestion() {
+            let numRandom = Math.floor(Math.random() * questionArr.length);
+            questionArr[numRandom].displayQuestion();
+
+            let answer = prompt("Please select the correct answer");
+
+            if(answer !== 'exit') {
+               questionArr[numRandom].checkAnswer(parseInt(answer));
+           
+                nextQuestion();
+            }
+        }
+   nextQuestion();
+})();
