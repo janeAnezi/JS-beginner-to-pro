cc5272d549396efacaa4138ce6217de1a0d2edba
Anezijane <ahnnieanezi@gmail.com>
2024-05-13 13:29:42 +0100

    add forEach loop method

--- Basic-js/loopsAndIteration.js
+++ Basic-js/loopsAndIteration.js
@@ -39,3 +39,32 @@ const x = [0,1,2,3,]
 x.forEach(num => {
     console.log(num + 1)
 });
+
+let array = [1, 2, 3, 4, 5];
+
+array.forEach(function(element, index, array) {
+  console.log(`Element ${element} at index ${index}`);
+});
+
+// Output:
+// Element 1 at index 0
+// Element 2 at index 1
+// Element 3 at index 2
+// Element 4 at index 3
+// Element 5 at index 4
+
+// looping over array of objects
+let people = [
+    { name: 'John', age: 30 },
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 35 }
+  ];
+  
+  people.forEach(function(person, index) {
+    console.log(`Person ${index + 1}: ${person.name} is ${person.age} years old`);
+  });
+  
+  // Output:
+  // Person 1: John is 30 years old
+  // Person 2: Alice is 25 years old
+  // Person 3: Bob is 35 years old
