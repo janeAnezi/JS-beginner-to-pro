6a23dd01d7dc5b2c52cecb63ed2799c88cb1dad7
Anezijane <ahnnieanezi@gmail.com>
2024-05-13 15:06:02 +0100

    check balance test

--- Basic-js/ifElseStatement.js
+++ Basic-js/ifElseStatement.js
@@ -23,3 +23,24 @@ if(weather == "rainny") {
 } else {
     console.log("Wear your sun glasses 😎");
 }
+
+// Bank account condition 
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = -300;
+var checkBalance = true;
+var isActive = true;
+
+// your code goes here
+if(checkBalance === true) {
+    if(isActive === true && balance > 0) {
+        console.log(`Your balance is $${balance.toFixed(2)}.`);
+    }else if(isActive === true && balance === 0) {
+        console.log(`Your account is empty.`);    
+    }else if(isActive === true && balance < 0) {
+        console.log (`Your balance is negative, Please contact your bank.`)   
+    }else if(isActive === false) {
+        console.log(`Your account is no longer active.`);
+    }
+}else {
+    console.log(`Thank you, Have a nice day!`)
+}
